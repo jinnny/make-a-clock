0f14f210ad87c8f393e09485f418936ac3ecb000
Jinny <jinny920311@gmail.com>
2019-01-12 22:28:39 +0900

    clock

---
+++ clock.js
@@ -0,0 +1,19 @@
+const clockContainer = document.querySelector('.js-clock'),
+    clockTitle = clockContainer.querySelector('.js-clock-title');
+
+function getTime(){
+    const date = new Date(),
+     minutes = date.getMinutes(),
+     hours = date.getHours(),
+     seconds= date.getSeconds();
+     clockTitle.innerHTML = `${
+         hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}`: minutes}:${
+         seconds < 10 ? `0${seconds}` : seconds 
+    }`;
+}
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
